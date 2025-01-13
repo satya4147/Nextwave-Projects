d8b0cc3f09d44926cfb2f670a611eba043c1dcb3
satya4147 <m.satya4147@gmail.com>
2025-01-13 15:46:18 +0530

    digital_timer_app

---
+++ React/digital_timer_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DigitalTimer from './DigitalClock';
+
+function App() {
+  return (
+    <div className="App">
+      <DigitalTimer />
+    </div>
+  );
+}
+
+export default App;
