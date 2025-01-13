8daf5c62470c61b8364a0e15cd00a30d75ffcf21
satya4147 <m.satya4147@gmail.com>
2025-01-13 15:05:28 +0530

    comments_app

---
+++ React/comments_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Comments from './Component/CommentList';
+
+function App() {
+  return (
+    <div className="App">
+     <Comments />
+    </div>
+  );
+}
+
+export default App;
